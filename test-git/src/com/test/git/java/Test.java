a830b5b8e4a66f523a503f4c5ec409edd9f28c56
rashmik <rashmi.kandakur@gmail.com>
2013-01-15 10:25:36 +0000

    changed sysout

--- test-git/src/com/test/git/java/Test.java
+++ test-git/src/com/test/git/java/Test.java
@@ -7,7 +7,7 @@ public class Test {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello new branch 1");
+		System.out.println("hello new branch 2");
 		System.out.println("successfully created 2nd new branch");
 	}
 
