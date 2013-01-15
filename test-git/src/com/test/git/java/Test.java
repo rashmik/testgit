861408125b3af387d636beae2dc47651319ba661
rashmik <rashmi.kandakur@gmail.com>
2013-01-15 10:58:19 +0000

    10.58AM

--- test-git/src/com/test/git/java/Test.java
+++ test-git/src/com/test/git/java/Test.java
@@ -7,7 +7,7 @@ public class Test {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello new branch 2 -not able to check out new changes - 10.51");
+		System.out.println("hello new branch 2 -not able to check out new changes - 10.58");
 		System.out.println("successfully created 2nd new branch");
 	}
 
