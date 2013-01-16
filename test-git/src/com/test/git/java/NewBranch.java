a6f8c5787abf658629a3e4a9497da2eb7ec713c3
rashmik <rashmi.kandakur@gmail.com>
2013-01-16 15:51:35 +0000

    new commit to new file in new branch - 15.51PM

--- test-git/src/com/test/git/java/NewBranch.java
+++ test-git/src/com/test/git/java/NewBranch.java
@@ -8,6 +8,7 @@ public class NewBranch {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("adding new file to new branch");
+		System.out.println("New change to new file in new branch");
 	}
 
 }
