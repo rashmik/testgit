3db46d82c25c0cb73741e5ff9ebd935838d553fc
rashmik <rashmi.kandakur@gmail.com>
2013-01-31 12:21:33 +0000

    testing commit from tortise 12.24

--- test-git/src/com/test/git/java/NewBranch.java
+++ test-git/src/com/test/git/java/NewBranch.java
@@ -14,6 +14,7 @@ public class NewBranch {
 		System.out.println("11.21AM");
 		System.out.println("12.19PM");
 		System.out.println("12.20PM");
+		System.out.println("testing commit from tortise");
 	}
 
 }
