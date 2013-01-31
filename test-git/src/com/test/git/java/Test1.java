c2164b1724909e7a35b893e7b0052f6dee55caf6
rashmik <rashmi.kandakur@gmail.com>
2013-01-31 10:16:43 +0000

    creating new file - 10.19AM

---
+++ test-git/src/com/test/git/java/Test1.java
@@ -0,0 +1,13 @@
+package com.test.git.java;
+
+public class Test1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("creating new file,not commiting now");
+	}
+
+}
