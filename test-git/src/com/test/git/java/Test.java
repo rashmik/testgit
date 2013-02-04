7328a06d2865c5f6dec1cc32cdf04e7187bac256
rashmik <rashmi.kandakur@gmail.com>
2013-02-04 13:13:29 +0000

    Testing rebase- 13.09PM

--- test-git/src/com/test/git/java/Test.java
+++ test-git/src/com/test/git/java/Test.java
@@ -9,6 +9,7 @@ public class Test {
 		// TODO Auto-generated method stub
 		System.out.println("hello GIT");
 		System.out.println("merging testing");
+		System.out.println("testing rebase");
 	}
 
 }
