2868368c88870d34c03e4322c964cb58d2621715
rashmik <rashmi.kandakur@gmail.com>
2013-02-05 09:42:31 +0000

    final chk : 09:41AM

--- test-git/src/com/test/git/java/Addition.java
+++ test-git/src/com/test/git/java/Addition.java
@@ -8,6 +8,8 @@ public class Addition {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("adding new file");
+		System.out.println("09:41AM");
+		
 	}
 
 }
