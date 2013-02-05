817cdfc0abe8807bc9a9ef38e76d221641d13b00
rashmik <rashmi.kandakur@gmail.com>
2013-02-05 09:44:31 +0000

    multiple commits on single file.:09:43AM

--- test-git/src/com/test/git/java/Addition.java
+++ test-git/src/com/test/git/java/Addition.java
@@ -9,6 +9,7 @@ public class Addition {
 		// TODO Auto-generated method stub
 		System.out.println("adding new file");
 		System.out.println("09:41AM");
+		System.out.println("testing multiple commits for a single file : 09:43AM");
 		
 	}
 
