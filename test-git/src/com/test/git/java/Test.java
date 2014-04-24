acfd883ca3b7298526ce445831b3dba38076645f
rashmik <rashmi.kandakur@gmail.com>
2014-04-24 13:08:59 +0100

    Test commit 1

--- test-git/src/com/test/git/java/Test.java
+++ test-git/src/com/test/git/java/Test.java
@@ -8,7 +8,7 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello");
-		
+		System.out.println("hello");
 		System.out.println("This is a master copy branch");
 		System.out.println("testing uncommiting file within same branch and at branch level");
 		System.out.println("Test");
